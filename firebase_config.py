273e6e7b4d8e95e87530c93e05632ae189b8ccc9
harpyeagle-dev <55797063+harpyeagle-dev@users.noreply.github.com>
2025-04-01 13:57:40 -0400

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,25 +1,27 @@
-# firebase_config.py
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials, db
 import streamlit as st
 
-# Load Firebase credentials from secrets
-db = None
+def initialize_firebase():
+    if not firebase_admin._apps:
+        cred = credentials.Certificate({
+            "type": st.secrets.FIREBASE.type,
+            "project_id": st.secrets.FIREBASE.project_id,
+            "private_key_id": st.secrets.FIREBASE.private_key_id,
+            "private_key": st.secrets.FIREBASE.private_key.replace("\\n", "\n"),
+            "client_email": st.secrets.FIREBASE.client_email,
+            "client_id": st.secrets.FIREBASE.client_id,
+            "auth_uri": st.secrets.FIREBASE.auth_uri,
+            "token_uri": st.secrets.FIREBASE.token_uri,
+            "auth_provider_x509_cert_url": st.secrets.FIREBASE.auth_provider_x509_cert_url,
+            "client_x509_cert_url": st.secrets.FIREBASE.client_x509_cert_url
+        })
 
-if not firebase_admin._apps:
-    cred = credentials.Certificate({
-        "type": st.secrets["firebase"]["type"],
-        "project_id": st.secrets["firebase"]["project_id"],
-        "private_key_id": st.secrets["firebase"]["private_key_id"],
-        "private_key": st.secrets["firebase"]["private_key"].replace("\\n", "\n"),
-        "client_email": st.secrets["firebase"]["client_email"],
-        "client_id": st.secrets["firebase"]["client_id"],
-        "auth_uri": st.secrets["firebase"]["auth_uri"],
-        "token_uri": st.secrets["firebase"]["token_uri"],
-        "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
-        "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"]
-    })
-    firebase_admin.initialize_app(cred)
+        firebase_admin.initialize_app(cred, {
+            'databaseURL': st.secrets.FIREBASE.database_url
+        })
 
-# Connect to Firestore
-db = firestore.client()
+    return db
+
+# Call this in your main app file to get the db connection
+db = initialize_firebase()
