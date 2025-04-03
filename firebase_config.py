63ca365c114a2aa47e52df0012b0946f69f5138d
harpyeagle-dev <55797063+harpyeagle-dev@users.noreply.github.com>
2025-04-03 11:13:44 -0400

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -19,7 +19,7 @@ def initialize_firebase():
                 "client_x509_cert_url": st.secrets["FIREBASE"]["client_x509_cert_url"]
             })
             firebase_admin.initialize_app(cred, {
-                'databaseURL': st.secrets["FIREBASE"]["https://guyana-voters-pulse-default-rtdb.firebaseio.com"]
+                'databaseURL': st.secrets["FIREBASE"]["database_url"]
             })
             st.success("✅ Firebase initialized.")
         return db
